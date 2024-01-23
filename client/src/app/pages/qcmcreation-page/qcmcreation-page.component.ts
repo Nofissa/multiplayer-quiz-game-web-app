8186af204e50db0be069d2e46408e1cc376cda2f
R0ma01 <romaine.brand@polymtl.ca>
2024-01-22 22:51:28 -0500

    créer un nouveau composant qcm creation pour la page de création de qcm

---
+++ client/src/app/pages/qcmcreation-page/qcmcreation-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-qcmcreation-page',
+    templateUrl: './qcmcreation-page.component.html',
+    styleUrls: ['./qcmcreation-page.component.scss'],
+})
+export class QCMCreationPageComponent {
+    title = 'hi';
+}
