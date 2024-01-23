152242782c171bbee4b49c05d0d4228cd6b19e5e
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-01-23 01:39:16 -0500

    feat: added backend interactions for questions

---
+++ server/app/controllers/question/question.controller.ts
@@ -0,0 +1,80 @@
+import { Course } from '@app/model/database/course';
+import { CreateQuestionDto } from '@app/model/dto/question/create-question.dto';
+import { UpdateQuestionDto } from '@app/model/dto/question/update-question.dto';
+import { QuestionService } from '@app/services/question/question.service';
+import { Body, Controller, Get, HttpStatus, Put, Post, Res, Delete, Param } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+
+@ApiTags('Questions')
+@Controller('questions')
+export class QuestionController {
+    constructor(private readonly questionService: QuestionService) {}
+
+    @ApiOkResponse({
+        description: 'Returns all questions',
+        type: Course,
+        isArray: true,
+    })
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+    @Get('/')
+    async getAllQuestions(@Res() response: Response) {
+        try {
+            const allQuestions = await this.questionService.getAllQuestions();
+            response.status(HttpStatus.OK).json(allQuestions);
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }
+    }
+
+    @ApiCreatedResponse({
+        description: 'Add new question',
+    })
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+    @Post('/')
+    async addQuestion(@Body() dto: CreateQuestionDto, @Res() response: Response) {
+        try {
+            await this.questionService.addQuestion(dto);
+            response.status(HttpStatus.CREATED).send();
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }
+    }
+
+    @ApiOkResponse({
+        description: 'Modify a question',
+        type: Course,
+    })
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+    @Put('/')
+    async modifyQuestion(@Body() dto: UpdateQuestionDto, @Res() response: Response) {
+        try {
+            await this.questionService.modifyQuestion(dto);
+            response.status(HttpStatus.OK).send();
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }
+    }
+
+    @ApiOkResponse({
+        description: 'Delete a question',
+    })
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+    @Delete('/:id')
+    async deleteQuestionById(@Param('id') id: string, @Res() response: Response) {
+        try {
+            await this.questionService.deleteQuestionById(id);
+            response.status(HttpStatus.OK).send();
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }
+    }
+}
