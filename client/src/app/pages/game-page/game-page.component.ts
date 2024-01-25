be0b5d92bd18d3293a18375888f3b2d859e6a7fe
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-01-25 10:29:31 -0500

    HostListener error fixed + validating choices now working

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component, HostListener } from '@angular/core';
-import { map, take, timer } from 'rxjs';
+import { Subscription, map, take, timer } from 'rxjs';
 
 @Component({
     selector: 'app-game-page',
@@ -7,30 +8,44 @@ import { map, take, timer } from 'rxjs';
     styleUrls: ['./game-page.component.scss'],
 })
 export class GamePageComponent {
-    @HostListener('keydown', ['$event'])
     // message: BehaviorSubject<string> = new BehaviorSubject<string>('');
-    secondsLeft: number;
+    secondsLeft: number = 0;
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     frequenceOneSecond: number = 1000;
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     timerDuration: number = 60;
     selectedAnswerBoxes: number[] = [];
+    validatedAnswerBoxes: number[] = [];
     firstBoxHotkey: string = '1';
     secondBoxHotkey: string = '2';
     thirdBoxHotkey: string = '3';
     fourthBoxHotkey: string = '4';
+    private timerSubscription: Subscription;
 
     // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
     ngOnInit() {
-        // timer code source: https://www.codeproject.com/Questions/5349203/How-to-make-5-minute-countdown-timer-with-rxjs-and
+        this.startTimer();
+    }
+
+    // timer inspired from ChatGPT and https://www.codeproject.com/Questions/5349203/How-to-make-5-minute-countdown-timer-with-rxjs-and
+    startTimer() {
         const countdown$ = timer(0, this.frequenceOneSecond).pipe(
-            take(this.timerDuration),
+            take(this.timerDuration + 1),
             map((secondsElapsed) => this.timerDuration - secondsElapsed),
         );
 
-        countdown$.subscribe((secondsLeft: number) => {
+        this.timerSubscription = countdown$.subscribe((secondsLeft: number) => {
             this.secondsLeft = secondsLeft;
+            if (this.secondsLeft === 0) {
+                this.validateChoices();
+            }
         });
+    }
+
+    stopTimer() {
+        if (this.timerSubscription && !this.timerSubscription.closed) {
+            this.timerSubscription.unsubscribe();
+        }
     }
 
     isSelected(answerBoxNumber: number): boolean {
@@ -45,6 +60,27 @@ export class GamePageComponent {
         }
     }
 
+    validateChoices() {
+        for (const box of this.selectedAnswerBoxes) {
+            const selectedBox = document.getElementsByClassName('answer-box' + box);
+            selectedBox[0].classList.remove('highlight-selected');
+            selectedBox[0].classList.add('highlight-validated');
+            this.validatedAnswerBoxes.push(box);
+        }
+        if (this.validatedAnswerBoxes.length !== 0) {
+            const box1 = document.getElementsByClassName('answer-box1');
+            box1[0].classList.add('disable-click');
+            const box2 = document.getElementsByClassName('answer-box2');
+            box2[0].classList.add('disable-click');
+            const box3 = document.getElementsByClassName('answer-box3');
+            box3[0].classList.add('disable-click');
+            const box4 = document.getElementsByClassName('answer-box4');
+            box4[0].classList.add('disable-click');
+            this.stopTimer();
+        }
+    }
+
+    @HostListener('keydown', ['$event'])
     handleKeyboardEvent(event: KeyboardEvent): void {
         switch (event.key) {
             case this.firstBoxHotkey: {
@@ -65,6 +101,10 @@ export class GamePageComponent {
             case this.fourthBoxHotkey: {
                 event.preventDefault();
                 this.toggleAnswerBox(parseInt(this.fourthBoxHotkey, 10));
+                break;
+            }
+            case 'Enter': {
+                this.validateChoices();
                 break;
             }
             default: {
