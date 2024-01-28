a42604105d6b26c42713e2dd7f8bcb7af95d6460
R0ma01 <romaine.brand@polymtl.ca>
2024-01-28 01:43:00 -0500

    ajout de la range bar pour le pointage des questions

--- client/src/app/components/create-question-modal/create-question-modal.component.ts
+++ client/src/app/components/create-question-modal/create-question-modal.component.ts
@@ -22,37 +22,50 @@ export class CreateQuestionModalComponent {
         if (modal) modal.setAttribute('display', 'flex');
     }
 
-    createPointSelectHTML(answerNumber: number) {
-        return answerNumber;
-    }
-
     createAnswerHTML(answerNumber: number) {
         const answerDiv = document.createElement('div');
         answerDiv.classList.add('flex-row');
         const answerArea = document.createElement('textarea');
         const questionNumber = document.createElement('p');
         const checkBox = document.createElement('input');
-        checkBox.setAttribute('type', 'checkbox');
+        const rangeBar = document.createElement('input');
+
+        checkBox.type = 'checkbox';
         checkBox.name = 'trueOrFalse';
         checkBox.value = `${answerNumber}`;
+
+        rangeBar.type = 'range';
+        rangeBar.style.display = 'none';
+
+        checkBox.addEventListener('click', () => {
+            if (rangeBar.style.display !== 'none') {
+                rangeBar.style.display = 'none'; // Hide the range bar
+            } else {
+                rangeBar.style.display = 'flex'; // Show the range bar
+            }
+        });
+
         questionNumber.textContent = '# ' + answerNumber;
+
         answerArea.id = 'answer' + answerNumber;
+
         answerDiv.appendChild(questionNumber);
         answerDiv.appendChild(answerArea);
         answerDiv.appendChild(checkBox);
+        answerDiv.appendChild(rangeBar);
         return answerDiv;
     }
 
     setUpModal() {
         const answersDiv = document.getElementById('modal-answers-container');
         if (answersDiv) {
-            while (answersDiv.children.length > this.selectedNumber) {
+            while (answersDiv.children.length + 1 > this.selectedNumber) {
                 const child = answersDiv.lastChild;
                 if (child) {
                     answersDiv.removeChild(child);
                 }
             }
-            for (let i = answersDiv.children.length; i < this.selectedNumber; i++) {
+            for (let i = answersDiv.children.length + 1; i <= this.selectedNumber; i++) {
                 answersDiv.appendChild(this.createAnswerHTML(i));
             }
         }
