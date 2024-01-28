4c1ee7abd5eed42e96cc9232026ec75067218b4d
lorisponderant <loris.ponderant@polymtl.ca>
2024-01-28 15:01:17 -0500

    Removing the question part of the server

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,11 +1,8 @@
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { QuestionController } from './controllers/question/question.controller';
 import { QuizController } from './controllers/quiz/quiz.controller';
-import { Question, questionSchema } from './model/database/question';
 import { Quiz, quizSchema } from './model/database/quiz';
-import { QuestionService } from './services/question/question.service';
 import { QuizService } from './services/quiz/quiz.service';
 
 @Module({
@@ -18,9 +15,9 @@ import { QuizService } from './services/quiz/quiz.service';
                 uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
             }),
         }),
-        MongooseModule.forFeature([{ name: Question.name, schema: questionSchema},{ name: Quiz.name, schema: quizSchema }]),
+        MongooseModule.forFeature([{ name: Quiz.name, schema: quizSchema }]),
     ],
-    controllers: [QuestionController, QuizController],
-    providers: [QuestionService, QuizService, Logger],
+    controllers: [QuizController],
+    providers: [QuizService, Logger],
 })
 export class AppModule {}
