18b2c44d2fc46041a11c364fe54ebf5e8cc07b7e
lorisponderant <loris.ponderant@polymtl.ca>
2024-01-28 14:20:36 -0500

    partie serveur quiz almost done

--- server/app/controllers/quiz/quiz.controller.ts
+++ server/app/controllers/quiz/quiz.controller.ts
@@ -1,4 +1,5 @@
 import { Course } from '@app/model/database/course';
+import { Question, Quiz } from '@app/model/database/quiz';
 import { UpsertQuizDto } from '@app/model/dto/quiz/upsert-quiz.dto';
 import { QuizService } from '@app/services/quiz/quiz.service';
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
@@ -35,7 +36,7 @@ export class QuizController {
         description: 'Return NOT_FOUND http status when request fails',
     })
     @Post('/')
-    async addQuestion(@Body() dto: UpsertQuizDto, @Res() response: Response) {
+    async addQuiz(@Body() dto: UpsertQuizDto, @Res() response: Response) {
         try {
             await this.quizService.addQuiz(dto);
             response.status(HttpStatus.CREATED).send();
@@ -52,13 +53,52 @@ export class QuizController {
         description: 'Return NOT_FOUND http status when request fails',
     })
     @Put('/')
-    async modifyQuestion(@Body() dto: UpsertQuizDto, @Res() response: Response) {
+    async modifyQuiz(@Body() dto: UpsertQuizDto, @Res() response: Response) {
         try {
-            await this.quizService.modifyQuiz(dto);
-            response.status(HttpStatus.OK).send();
+            const quiz : Quiz = await this.quizService.modifyQuiz(dto);
+            response.status(HttpStatus.OK).json(quiz);
         } catch (error) {
             response.status(HttpStatus.NOT_FOUND).send(error.message);
         }
+    }
+
+    @ApiOkResponse({
+        description: 'Modify a question in a quiz',
+        type: Course,
+    })
+
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+
+    @Put('/:id/questions/:questionId')
+    async modifyQuestionInQuiz(@Param('id') quizId: string,@Param('questionId') questionId :string, @Res() response: Response) {
+        try {
+            const question : Question = await this.quizService.modifyQuestionInQuiz(quizId, questionId);
+            response.status(HttpStatus.OK).json(question);
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }
+    }
+
+    @ApiOkResponse({
+        description: 'Delete a question in a quiz ',
+        type: Course,
+    })
+
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+
+    @Delete('/:id/questions/:questionId')
+
+    async deleteQuestionInQuiz(@Param('id') quizId: string,@Param('questionId') questionId :string, @Res() response: Response) {
+        try {
+            const question : Question = await this.quizService.deleteQuestionInQuizbyId(quizId, questionId);
+            response.status(HttpStatus.OK).json(question);
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }   
     }
 
     @ApiOkResponse({
@@ -68,12 +108,12 @@ export class QuizController {
         description: 'Return NOT_FOUND http status when request fails',
     })
     @Delete('/:id')
-    async deleteQuestionById(@Param('id') id: string, @Res() response: Response) {
+    async deleteQuizById(@Param('id') id: string, @Res() response: Response) {
         try {
-            await this.quizService.deleteQuestionById(id);
-            response.status(HttpStatus.OK).send();
+            const quiz: Quiz = await this.quizService.deleteQuizById(id);
+            response.status(HttpStatus.OK).json(quiz);
         } catch (error) {
-            response.status(HttpStatus.NOT_FOUND).send(error.message);
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error.message);
         }
     }
 }
---
+++ server/app/model/database/question.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document } from 'mongoose';
+
+export type QuestionDocument = Question & Document;
+
+@Schema()
+export class Question {
+    @ApiProperty()
+    @Prop({ required: true })
+    question: string;
+
+    @ApiProperty()
+    @Prop({ required: true })
+    incorrectAnswers: string[];
+
+    @ApiProperty()
+    @Prop({ required: true })
+    correctAnswer: string[];
+
+    @ApiProperty()
+    @Prop({ required: true })
+    lastModified: Date;
+
+    @ApiProperty()
+    _id?: string;
+}
+
+export const questionSchema = SchemaFactory.createForClass(Question);
