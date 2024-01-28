b17b5d2e80e97e0980afacc0d8a688c43b7929d5
R0ma01 <romaine.brand@polymtl.ca>
2024-01-28 12:44:24 -0500

    utilisation de form control, conversion des fichiers

--- client/src/app/components/create-question-modal/create-question-modal.component.ts
+++ client/src/app/components/create-question-modal/create-question-modal.component.ts
@@ -1,91 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-create-question-modal',
     templateUrl: './create-question-modal.component.html',
     styleUrls: ['./create-question-modal.component.scss'],
 })
-export class CreateQuestionModalComponent {
-    selectedNumber: number = 0;
-    submitNewQeustion() {
-        window.console.log('i function');
+export class CreateQuestionModalComponent implements OnInit {
+    questionForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    get answers(): FormArray {
+        return this.questionForm.get('answers') as FormArray;
+    }
+
+    ngOnInit() {
+        this.questionForm = this.formBuilder.group({
+            question: ['', Validators.required],
+            selectedNumber: [2, Validators.required], // Default value is 2
+            answers: this.formBuilder.array([]),
+        });
+    }
+
+    submitNewQuestion() {
+        window.console.log('Form submitted:', this.questionForm.value);
         this.closeModal();
     }
 
+    setUpModal() {
+        let numberOfAnswers;
+        const selectedNumber = this.questionForm.get('selectedNumber');
+        if (selectedNumber) {
+            numberOfAnswers = selectedNumber.value;
+        }
+        const answersArray = this.questionForm.get('answers') as FormArray;
+
+        while (answersArray.length < numberOfAnswers) {
+            answersArray.push(
+                this.formBuilder.group({
+                    answerText: ['', Validators.required],
+                    isCorrect: false,
+                    rangeValue: [0, Validators.required],
+                }),
+            );
+        }
+
+        while (answersArray.length > numberOfAnswers) {
+            answersArray.removeAt(answersArray.length - 1);
+        }
+    }
+
     closeModal() {
-        const modal = document.getElementById('question-creation-modal');
-        if (modal) modal.setAttribute('display', 'block');
+        // Your existing closeModal logic
     }
 
     openModal() {
-        const modal = document.getElementById('question-creation-modal');
-        if (modal) modal.setAttribute('display', 'flex');
-    }
-
-    createAnswerHTML(answerNumber: number) {
-        const answerDiv = document.createElement('div');
-        answerDiv.classList.add('flex-row');
-        const answerArea = document.createElement('textarea');
-        const questionNumber = document.createElement('p');
-        const checkBox = document.createElement('input');
-        const rangeBar = document.createElement('input');
-
-        const minLabel = document.createElement('span');
-        const maxLabel = document.createElement('span');
-
-        checkBox.type = 'checkbox';
-        checkBox.name = 'trueOrFalse';
-        checkBox.value = `${answerNumber}`;
-
-        rangeBar.type = 'range';
-        rangeBar.style.display = 'none';
-        rangeBar.min = '0';
-        rangeBar.max = '100';
-        rangeBar.step = '10';
-
-        minLabel.textContent = '0';
-        maxLabel.textContent = '100';
-        minLabel.style.display = 'none';
-        maxLabel.style.display = 'none';
-
-        checkBox.addEventListener('click', () => {
-            if (rangeBar.style.display !== 'none') {
-                rangeBar.style.display = 'none';
-                minLabel.style.display = 'none';
-                maxLabel.style.display = 'none';
-            } else {
-                rangeBar.style.display = 'flex';
-                minLabel.style.display = 'flex';
-                maxLabel.style.display = 'flex';
-            }
-        });
-
-        questionNumber.textContent = '# ' + answerNumber;
-
-        answerArea.id = 'answer' + answerNumber;
-
-        answerDiv.appendChild(questionNumber);
-        answerDiv.appendChild(answerArea);
-        answerDiv.appendChild(checkBox);
-        answerDiv.appendChild(minLabel);
-        answerDiv.appendChild(rangeBar);
-        answerDiv.appendChild(maxLabel);
-        return answerDiv;
-    }
-
-    setUpModal() {
-        const answersDiv = document.getElementById('modal-answers-container');
-        if (answersDiv) {
-            while (answersDiv.children.length + 1 > this.selectedNumber) {
-                const child = answersDiv.lastChild;
-                if (child) {
-                    answersDiv.removeChild(child);
-                }
-            }
-            for (let i = answersDiv.children.length + 1; i <= this.selectedNumber; i++) {
-                answersDiv.appendChild(this.createAnswerHTML(i));
-            }
-        }
-        this.selectedNumber = 0;
+        // Your existing openModal logic
     }
 }
