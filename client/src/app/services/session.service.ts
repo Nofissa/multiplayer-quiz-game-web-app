63982ada888c5e2467e856b0a2baf46934add9c0
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-01-30 13:58:26 -0500

    feature: added session service for handling sessions

---
+++ client/src/app/services/session.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SessionService {
+    private readonly tokenKey = 'authToken';
+
+    setSession(token: string) {
+        localStorage.setItem(this.tokenKey, token);
+    }
+
+    getSession(): string | null {
+        return localStorage.getItem(this.tokenKey);
+    }
+
+    clearSession() {
+        localStorage.removeItem(this.tokenKey);
+    }
+}
