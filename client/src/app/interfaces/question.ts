2f0e303154ce08ba235afa4cdd9780b9ec14d444
Dimitri <dimi.mansour03@gmail.com>
2024-02-01 14:33:01 -0500

    fix: choice uppercase

--- client/src/app/interfaces/question.ts
+++ client/src/app/interfaces/question.ts
@@ -1,11 +1,11 @@
 import { QuestionType } from '../../../../common/question-type';
-import { choice } from './choice';
+import { Choice } from './choice';
 
 export interface Question {
     type: QuestionType;
     text: string;
     points: number;
-    choices: choice[];
+    choices: Choice[];
     lastModification: Date | null;
     _id: string;
 }
