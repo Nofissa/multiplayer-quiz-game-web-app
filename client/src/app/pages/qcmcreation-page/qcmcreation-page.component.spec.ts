f21819384a7d0ce8e22c3c244e52a43c601222c7
R0ma01 <romaine.brand@polymtl.ca>
2024-02-01 09:56:27 -0500

    Passé le linter dnas le fichier

--- client/src/app/pages/qcmcreation-page/qcmcreation-page.component.spec.ts
+++ client/src/app/pages/qcmcreation-page/qcmcreation-page.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { QCMCreationPageComponent } from './qcmcreation-page.component';
 
 describe('QCMCreationPageComponent', () => {
-  let component: QCMCreationPageComponent;
-  let fixture: ComponentFixture<QCMCreationPageComponent>;
+    let component: QCMCreationPageComponent;
+    let fixture: ComponentFixture<QCMCreationPageComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [QCMCreationPageComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [QCMCreationPageComponent],
+        });
+        fixture = TestBed.createComponent(QCMCreationPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(QCMCreationPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
