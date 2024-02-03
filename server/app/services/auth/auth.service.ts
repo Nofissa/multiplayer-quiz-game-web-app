e2138673dde5dfe03421050b834c9a49d98d496c
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-02-02 23:04:03 -0500

    fix: added try catch blocks to auth service

--- server/app/services/auth/auth.service.ts
+++ server/app/services/auth/auth.service.ts
@@ -9,26 +9,34 @@ const usersFilePath = 'assets/users.json';
 @Injectable()
 export class AuthService {
     async authenticate(userCredentialSet: UserCredentialSet): Promise<AuthPayload> {
-        const userCredentialSets: UserCredentialSet[] = JSON.parse(readFileSync(usersFilePath).toString());
-        const matchingCredentialSet: UserCredentialSet = userCredentialSets.find((x) => {
-            return x.username === userCredentialSet.username && x.password === userCredentialSet.password;
-        });
+        try {
+            const userCredentialSets: UserCredentialSet[] = JSON.parse(readFileSync(usersFilePath).toString());
+            const matchingCredentialSet: UserCredentialSet = userCredentialSets.find((x) => {
+                return x.username === userCredentialSet.username && x.password === userCredentialSet.password;
+            });
 
-        if (!matchingCredentialSet) {
-            return Promise.reject('Invalid credentials');
+            if (!matchingCredentialSet) {
+                return Promise.reject('Invalid credentials');
+            }
+
+            return {
+                token: sign({}, process.env.PRIVATE_RSA_KEY, { algorithm: 'RS256' }),
+            };
+        } catch (error) {
+            return Promise.reject(`Failed to authenticate: ${error}`);
         }
-
-        return {
-            token: sign({}, process.env.PRIVATE_RSA_KEY, { expiresIn: '6h', algorithm: 'RS256' }),
-        };
     }
 
     async verifyAuth(payload: AuthPayload): Promise<void> {
-        const token = payload.token;
-        if (!token) {
-            Promise.reject('No token in payload');
-        }
+        try {
+            const token = payload.token;
+            if (!token) {
+                Promise.reject('No token in payload');
+            }
 
-        verify(token, process.env.PUBLIC_RSA_KEY);
+            verify(token, process.env.PUBLIC_RSA_KEY);
+        } catch (error) {
+            return Promise.reject(`Failed to verify token: ${error}`);
+        }
     }
 }
