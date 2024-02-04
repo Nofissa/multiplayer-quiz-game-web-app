6c040c3a7225d52e4837097800f56e862237a5b7
Nofissa <90350596+Nofissa@users.noreply.github.com>
2024-02-04 13:44:41 -0500

    sync with dev

---
+++ client/src/app/components/dialogs/upsert-question-dialog/upsert-question-dialog/upsert-question-dialog.component.ts
@@ -0,0 +1,116 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Inject } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Choice } from '@app/interfaces/choice';
+import { Question } from '@app/interfaces/question';
+import { UpsertQuestionDialogData } from '@app/interfaces/upsert-question-dialog-data';
+
+const MAX_CHOICE_COUNT = 4;
+const POINT_VALUE_BASE_MULTIPLE = 10;
+
+@Component({
+    selector: 'app-upsert-question-dialog',
+    templateUrl: './upsert-question-dialog.component.html',
+    styleUrls: ['./upsert-question-dialog.component.scss'],
+})
+export class UpsertQuestionDialogComponent {
+    maxChoiceCount = MAX_CHOICE_COUNT;
+    formGroup: FormGroup;
+    choicesArray: FormArray;
+
+    // eslint-disable-next-line max-params
+    constructor(
+        private formBuilder: FormBuilder,
+        private snackBar: MatSnackBar,
+        public dialogRef: MatDialogRef<UpsertQuestionDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: UpsertQuestionDialogData,
+    ) {
+        this.choicesArray = this.formBuilder.array(
+            this.data.question.choices.map((answer) => {
+                return this.formBuilder.group({
+                    text: [answer.text, Validators.required],
+                    isCorrect: [answer.isCorrect, Validators.required],
+                });
+            }),
+            [Validators.required, this.oneFalseValidator(), this.oneTrueValidator()],
+        ) as FormArray<FormGroup>;
+
+        this.formGroup = this.formBuilder.group({
+            text: [this.data.question.text, Validators.required],
+            choices: this.choicesArray,
+            points: [this.data.question.points, [Validators.required, this.multipleOfTenValidator()]],
+        });
+    }
+
+    get choicesControls() {
+        return this.choicesArray.controls;
+    }
+
+    get choices() {
+        return this.formGroup.controls['choices'] as FormArray<FormGroup>;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    drop(event: CdkDragDrop<any[]>): void {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }
+
+    addAnswer() {
+        this.choicesArray.push(
+            this.formBuilder.group({
+                text: ['', Validators.required],
+                isCorrect: [false],
+            }),
+        );
+    }
+
+    removeAnswerAt(index: number) {
+        this.choicesArray.removeAt(index);
+    }
+
+    cancel() {
+        this.dialogRef.close();
+    }
+
+    submit() {
+        if (this.formGroup.valid) {
+            const question: Question = {
+                type: 'QCM',
+                text: this.formGroup.value.text,
+                points: this.formGroup.value.points,
+                choices: this.formGroup.value.choices,
+                lastModification: new Date(),
+                _id: '',
+            };
+
+            this.dialogRef.close(question);
+        } else {
+            this.snackBar.open("L'un des paramètres est erroné, veuillez réessayer", '', { duration: 2000 });
+        }
+    }
+
+    private oneTrueValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const answerArray: Choice[] = control.value;
+            const hasTrueAnswer = answerArray.some((answer) => answer.isCorrect);
+            return hasTrueAnswer ? null : { noTrueAnswer: true };
+        };
+    }
+
+    private oneFalseValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const answerArray: Choice[] = control.value;
+            const hasFalseAnswer = answerArray.some((answer) => !answer.isCorrect);
+            return hasFalseAnswer ? null : { noFalseAnswer: true };
+        };
+    }
+
+    private multipleOfTenValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const value = control.value;
+            return value % POINT_VALUE_BASE_MULTIPLE === 0 ? null : { notMultipleOfTen: true };
+        };
+    }
+}
---
+++ client/src/app/components/quiz/quiz/quiz.component.ts
@@ -0,0 +1,49 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Quiz } from '@app/interfaces/quiz';
+import { QuizHttpService } from '@app/services/quiz-http.service';
+import { saveAs } from 'file-saver';
+
+@Component({
+    selector: 'app-quiz',
+    templateUrl: './quiz.component.html',
+    styleUrls: ['./quiz.component.scss'],
+})
+export class QuizComponent {
+    @Input()
+    quiz: Quiz;
+
+    @Output()
+    refresh = new EventEmitter<void>();
+
+    isDeleted: boolean = false;
+
+    constructor(
+        private readonly quizHttpService: QuizHttpService,
+        private readonly router: Router,
+    ) {}
+
+    deleteQuiz() {
+        // eslint-disable-next-line no-underscore-dangle
+        this.quizHttpService.deleteQuizById(this.quiz._id).subscribe(() => {
+            this.isDeleted = true;
+        });
+    }
+
+    editQuiz() {
+        // eslint-disable-next-line no-underscore-dangle
+        this.router.navigate([`/qcm-creation?quizId=${this.quiz._id}`]);
+    }
+
+    exportQuiz() {
+        const blob = new Blob([JSON.stringify(this.quiz)], { type: 'text/json;charset=utf-8' });
+        saveAs(blob, `${this.quiz.title}.json`);
+    }
+
+    onToggleChange() {
+        // eslint-disable-next-line no-underscore-dangle
+        this.quizHttpService.hideQuizById(this.quiz._id).subscribe((quiz) => {
+            this.quiz = quiz;
+        });
+    }
+}
