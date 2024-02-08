a795197893573bed093a338fac940399715cbbb1
Dimitri <dimi.mansour03@gmail.com>
2024-02-08 15:27:04 -0500

    fix:beat the linting accusations

--- client/src/app/components/dialogs/prompt-dialog/prompt-dialog.component.spec.ts
+++ client/src/app/components/dialogs/prompt-dialog/prompt-dialog.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PromptDialogComponent } from './prompt-dialog.component';
 
 describe('PromptDialogComponent', () => {
-  let component: PromptDialogComponent;
-  let fixture: ComponentFixture<PromptDialogComponent>;
+    let component: PromptDialogComponent;
+    let fixture: ComponentFixture<PromptDialogComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PromptDialogComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [PromptDialogComponent],
+        });
+        fixture = TestBed.createComponent(PromptDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(PromptDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
