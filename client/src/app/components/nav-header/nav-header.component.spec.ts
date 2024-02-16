c413d163d2da026b75603e529b074702871eaee7
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-02-15 20:38:58 -0500

    hotfix: added tests for nav header

--- client/src/app/components/nav-header/nav-header.component.spec.ts
+++ client/src/app/components/nav-header/nav-header.component.spec.ts
@@ -1,21 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavHeaderComponent } from './nav-header.component';
+import { Location } from '@angular/common';
 
 describe('NavHeaderComponent', () => {
-  let component: NavHeaderComponent;
-  let fixture: ComponentFixture<NavHeaderComponent>;
+    let component: NavHeaderComponent;
+    let fixture: ComponentFixture<NavHeaderComponent>;
+    let locationSpy: jasmine.SpyObj<Location>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NavHeaderComponent]
+    beforeEach(() => {
+        const locationSpyObj = jasmine.createSpyObj('Location', ['back']);
+
+        TestBed.configureTestingModule({
+            declarations: [NavHeaderComponent],
+            providers: [{ provide: Location, useValue: locationSpyObj }],
+        });
+
+        fixture = TestBed.createComponent(NavHeaderComponent);
+        component = fixture.componentInstance;
+        locationSpy = TestBed.inject(Location) as jasmine.SpyObj<Location>;
     });
-    fixture = TestBed.createComponent(NavHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call location.back() in back', () => {
+        component.back();
+        expect(locationSpy.back).toHaveBeenCalled();
+    });
 });
