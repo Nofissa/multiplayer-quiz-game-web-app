27a802888b901b851b802798a25c74538b170048
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-02-18 22:43:39 -0500

    renamed files

--- client/src/app/components/timer-component/timer-component.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-timer-component',
-    templateUrl: './timer-component.component.html',
-    styleUrls: ['./timer-component.component.scss'],
-})
-export class TimerComponentComponent {}
