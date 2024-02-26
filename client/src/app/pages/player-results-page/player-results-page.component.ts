4e8036864581347a40740da58dbc77535cd4e1af
R0ma01 <romaine.brand@polymtl.ca>
2024-02-25 23:02:57 -0500

    formatting de histogramme data pour que choice ait la bonne forme.

--- client/src/app/pages/player-results-page/player-results-page.component.ts
+++ client/src/app/pages/player-results-page/player-results-page.component.ts
@@ -7,6 +7,7 @@ import { HistogrammeData } from '@app/interfaces/histogram-data';
     styleUrls: ['./player-results-page.component.scss'],
 })
 export class PlayerResultsPageComponent {
+    // temporary until web socket service for histogram is implemented
     answers: HistogrammeData[] = [
         {
             question: 'hee',
