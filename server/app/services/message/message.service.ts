dcc8ec2146c28d50e1bdf533ce48e19eaaf2a873
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-02-28 14:46:49 -0500

    hotfix: message service cuts to 200 char max per message

--- server/app/services/message/message.service.ts
+++ server/app/services/message/message.service.ts
@@ -3,6 +3,8 @@ import { Socket } from 'socket.io';
 import { Chatlog } from '@common/chatlog';
 import { GameService } from '@app/services/game/game.service';
 
+const MAX_MESSAGE_LENGTH = 200;
+
 @Injectable()
 export class MessageService {
     constructor(private readonly gameService: GameService) {}
@@ -10,7 +12,7 @@ export class MessageService {
     sendMessage(client: Socket, pin: string, message: string): Chatlog {
         const game = this.gameService.getGame(pin);
         const clientPlayer = game.clientPlayers.get(client.id);
-        const chatlog = { message, author: clientPlayer.player.username, date: new Date() };
+        const chatlog = { message: message.substring(0, MAX_MESSAGE_LENGTH), author: clientPlayer.player.username, date: new Date() };
 
         return chatlog;
     }
