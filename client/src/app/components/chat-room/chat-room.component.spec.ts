29ce651fa85ecc7f9a8128ec9ff258cc209e9a14
Nofissa <nofissa.khaif@polymtl.ca>
2024-02-27 22:25:02 -0500

    lint fix

--- client/src/app/components/chat-room/chat-room.component.spec.ts
+++ client/src/app/components/chat-room/chat-room.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChatRoomComponent } from './chat-room.component';
 
 describe('ChatRoomComponent', () => {
-  let component: ChatRoomComponent;
-  let fixture: ComponentFixture<ChatRoomComponent>;
+    let component: ChatRoomComponent;
+    let fixture: ComponentFixture<ChatRoomComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ChatRoomComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ChatRoomComponent],
+        });
+        fixture = TestBed.createComponent(ChatRoomComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(ChatRoomComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
