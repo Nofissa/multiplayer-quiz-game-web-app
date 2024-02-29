4bfc077fb29321642a43db5ef005686257f623ae
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-02-29 13:00:22 -0500

    back to http

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'https://ec2-3-96-124-77.ca-central-1.compute.amazonaws.com:3000',
-    apiUrl: 'https://ec2-3-96-124-77.ca-central-1.compute.amazonaws.com:3000/api',
+    serverUrl: 'http://ec2-3-96-124-77.ca-central-1.compute.amazonaws.com:3000',
+    apiUrl: 'http://ec2-3-96-124-77.ca-central-1.compute.amazonaws.com:3000/api',
 };
