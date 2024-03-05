4a7822d2a3ca61069f8151b5f6d0f1a60925dcd8
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-03-05 10:04:35 -0500

    uncomment tests and host game page initial layout

--- client/src/app/pages/host-game-page/host-game-page.component.spec.ts
+++ client/src/app/pages/host-game-page/host-game-page.component.spec.ts
@@ -1,21 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HostGamePageComponent } from './host-game-page.component';
 
 describe('HostGamePageComponent', () => {
-  let component: HostGamePageComponent;
-  let fixture: ComponentFixture<HostGamePageComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HostGamePageComponent]
+    let component: HostGamePageComponent;
+    let fixture: ComponentFixture<HostGamePageComponent>;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HostGamePageComponent],
+        });
+        fixture = TestBed.createComponent(HostGamePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(HostGamePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/pages/host-game-page/host-game-page.component.ts
+++ client/src/app/pages/host-game-page/host-game-page.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BarChartData } from '@app/interfaces/bar-chart-data';
+import { BarChartService } from '@app/services/game/bar-chart-service/bar-chart.service';
+import { GameService } from '@app/services/game/game-service/game.service';
+import { GameState } from '@common/game-state';
 
 @Component({
     selector: 'app-host-game-page',
     templateUrl: './host-game-page.component.html',
     styleUrls: ['./host-game-page.component.scss'],
 })
-export class HostGamePageComponent {}
+export class HostGamePageComponent implements OnInit {
+    pin: string;
+    isEnded: boolean = false;
+    gameState: GameState = GameState.Opened;
+
+    private barChartService: BarChartService = new BarChartService();
+
+    constructor(
+        private readonly gameService: GameService,
+        private readonly activatedRoute: ActivatedRoute,
+    ) {
+        this.gameService.onToggleSelectChoice(this.barChartService.updateBarChartData);
+        this.gameService.onNextQuestion(this.barChartService.addQuestion);
+        this.gameService.onToggleGameLock((gameState: GameState) => (this.gameState = gameState));
+    }
+
+    get barCharts(): BarChartData[] {
+        return this.barChartService.getAllBarChart();
+    }
+
+    get barChart(): BarChartData {
+        return this.barChartService.getLatestBarChart();
+    }
+
+    ngOnInit() {
+        this.pin = this.activatedRoute.snapshot.queryParams['pin'];
+    }
+
+    isLocked() {
+        return this.gameState === GameState.Closed;
+    }
+
+    isStarted() {
+        return this.gameState === GameState.Started;
+    }
+
+    toggleLock() {
+        this.gameService.toggleGameLock(this.pin);
+    }
+}
