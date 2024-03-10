6aac59ce272351a36b3b8faf8156e13d24a627ad
Dimitri Mansour <dimi.mansour03@gmail.com>
2024-03-09 21:59:58 -0500

    fix: added requested changes

--- client/src/app/components/player-list/player-list.component.ts
+++ client/src/app/components/player-list/player-list.component.ts
@@ -4,6 +4,8 @@ import { Player } from '@common/player';
 import { PlayerState } from '@common/player-state';
 import { Subscription } from 'rxjs';
 
+const NOT_FOUND_INDEX = -1;
+
 @Component({
     selector: 'app-player-list',
     templateUrl: './player-list.component.html',
@@ -19,25 +21,35 @@ export class PlayerListComponent implements OnInit {
     players: Player[] = [];
     playerJoinSub: Subscription;
     playerBanSub: Subscription;
-    playerAbdSub: Subscription;
-    private readonly gameService: GameService;
+    playerAbandonSub: Subscription;
+    questionFinished: Subscription;
 
-    constructor(gameService: GameService) {
-        this.gameService = gameService;
-    }
+    constructor(private readonly gameService: GameService) {}
 
     ngOnInit() {
         this.playerJoinSub = this.gameService.onJoinGame((payload) => {
             this.players = payload.players;
         });
-        this.playerAbdSub = this.gameService.onPlayerAbandon((player) => {
+        this.playerAbandonSub = this.gameService.onPlayerAbandon((player) => {
             const index = this.players.findIndex((p) => p.username === player.username);
-            this.players[index] = player;
+            if (index !== NOT_FOUND_INDEX) {
+                this.players[index] = player;
+            }
         });
         this.playerBanSub = this.gameService.onPlayerBan((player) => {
             const index = this.players.findIndex((p) => p.username === player.username);
-            this.players[index] = player;
+            if (index !== NOT_FOUND_INDEX) {
+                this.players[index] = player;
+            }
         });
+        /*
+        //TODO:
+        idealement faire un service qui detect quand la questions est fini puis utilise gameservice du server pour getGame()
+        this.questionFinished = this.gameService.onQuestionFinished((payload) => {
+            this.players = payload.game.players;
+            this.players.sort((a, b) => b.score - a.score);
+        });
+        */
     }
 
     banPlayer(player: Player) {
