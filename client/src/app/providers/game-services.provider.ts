c6a1bfdf216ccbd5a613bf10a554c8dd7671ecb2
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-03-11 13:14:02 -0400

    hotfix: removed game cache service

--- client/src/app/providers/game-services.provider.ts
+++ client/src/app/providers/game-services.provider.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
-import { GameCacheService } from '@app/services/game-cache/game-cache.service';
 import { GameService } from '@app/services/game/game-service/game.service';
 import { KeyBindingService } from '@app/services/key-binding/key-binding.service';
 import { TimerService } from '@app/services/timer/timer.service';
@@ -12,15 +11,10 @@ export class GameServicesProvider {
     // Disabled as this is a dependency provider class
     // eslint-disable-next-line max-params
     constructor(
-        private readonly gameCache: GameCacheService,
         private readonly game: GameService,
         private readonly timer: TimerService,
         private readonly keyBinding: KeyBindingService,
     ) {}
-
-    get gameCacheService(): GameCacheService {
-        return this.gameCache;
-    }
 
     get gameService(): GameService {
         return this.game;
