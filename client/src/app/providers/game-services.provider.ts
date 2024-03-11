c581e9c4a499fddeecadce636d53ad9676c6e62d
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-03-11 14:48:45 -0400

    feature: added game http service to game services provider

--- client/src/app/providers/game-services.provider.ts
+++ client/src/app/providers/game-services.provider.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
+import { GameHttpService } from '@app/services/game-http/game-http.service';
 import { GameService } from '@app/services/game/game-service/game.service';
 import { KeyBindingService } from '@app/services/key-binding/key-binding.service';
 import { TimerService } from '@app/services/timer/timer.service';
@@ -11,10 +12,15 @@ export class GameServicesProvider {
     // Disabled as this is a dependency provider class
     // eslint-disable-next-line max-params
     constructor(
+        private readonly gameHttp: GameHttpService,
         private readonly game: GameService,
         private readonly timer: TimerService,
         private readonly keyBinding: KeyBindingService,
     ) {}
+
+    get gameHttpService(): GameHttpService {
+        return this.gameHttp;
+    }
 
     get gameService(): GameService {
         return this.game;
