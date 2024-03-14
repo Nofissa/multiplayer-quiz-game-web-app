55e64583458f675978241f44a2bb951a67659e80
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-03-14 10:45:49 -0400

    added port

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-15-157-59-57.ca-central-1.compute.amazonaws.com',
-    apiUrl: 'http://ec2-15-157-59-57.ca-central-1.compute.amazonaws.com/api',
+    serverUrl: 'http://ec2-15-157-59-57.ca-central-1.compute.amazonaws.com:3000',
+    apiUrl: 'http://ec2-15-157-59-57.ca-central-1.compute.amazonaws.com:3000/api',
 };
