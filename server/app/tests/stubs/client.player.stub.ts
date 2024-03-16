4026b45d6b624f4760591f652002352ec7a33f96
lorisponderant <loris.ponderant@polymtl.ca>
2024-03-16 18:02:57 -0400

    fixing review issues

--- server/app/tests/stubs/client.player.stub.ts
+++ server/app/tests/stubs/client.player.stub.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */ // used for the socket (i only want the id here)
 import { ClientPlayer } from '@app/classes/client-player';
 import { playerstub } from './player.stub';
 
