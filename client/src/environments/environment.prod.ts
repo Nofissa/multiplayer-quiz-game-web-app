37e9db5e54884a1fd2413b551cb32a2a0ac19990
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-03-18 17:36:34 -0400

    hotfix: server error typo and client environment file

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ const serveurUrl = 'http://ec2-15-157-59-57.ca-central-1.compute.amazonaws.com';
 const serverPort = 3000;
 
 export const environment = {
-    production: false,
+    production: true,
     serverUrl: `${serveurUrl}:${serverPort}`,
     apiUrl: `${serveurUrl}:${serverPort}/api`,
 };
