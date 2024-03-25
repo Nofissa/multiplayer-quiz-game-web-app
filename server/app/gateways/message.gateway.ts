708e167cdf7441620b7f5546f81be4c83b027840
lorisponderant <loris.ponderant@polymtl.ca>
2024-03-24 22:07:10 -0400

    Timer and Message Getaway :white_check_mark:

---
+++ server/app/gateways/message.gateway.ts
@@ -0,0 +1,32 @@
+import { MessageService } from '@app/services/message/message.service';
+import { Chatlog } from '@common/chatlog';
+import { GameEventPayload } from '@common/game-event-payload';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+    cors: {
+        origin: ['http://localhost:4200', 'https://polytechnique-montr-al.gitlab.io', 'http://polytechnique-montr-al.gitlab.io'],
+        methods: ['GET', 'POST'],
+        transports: ['websocket', 'polling'],
+        credentials: false,
+    },
+})
+export class MessageGetaway {
+    @WebSocketServer()
+    server: Server;
+
+    constructor(private readonly messageService: MessageService) {}
+
+    @SubscribeMessage('sendMessage')
+    sendMessage(@ConnectedSocket() client: Socket, @MessageBody() { pin, message }: { pin: string; message: string }) {
+        try {
+            const chatlog = this.messageService.sendMessage(client, pin, message);
+            const payload: GameEventPayload<Chatlog> = { pin, data: chatlog };
+
+            this.server.to(pin).emit('sendMessage', payload);
+        } catch (error) {
+            client.emit('error', error.message);
+        }
+    }
+}
