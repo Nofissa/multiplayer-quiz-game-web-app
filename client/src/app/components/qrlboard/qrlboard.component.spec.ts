5b49b38d5d8b997e16219c246f3a826d4e8625d7
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-03-29 11:44:00 -0400

    fixed tests 80% coverage

--- client/src/app/components/qrlboard/qrlboard.component.spec.ts
+++ client/src/app/components/qrlboard/qrlboard.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testin
 import { FormBuilder } from '@angular/forms';
 import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { lastPlayerEvaluationStub } from '@app/TestStubs/evaluation.stubs';
@@ -16,12 +17,14 @@ import { PlayerService } from '@app/services/player/player.service';
 import { TimerService } from '@app/services/timer/timer.service';
 import { WebSocketService } from '@app/services/web-socket/web-socket.service';
 import { applyIfPinMatches } from '@app/utils/conditional-applications/conditional-applications';
-import { Evaluation } from '@common/evaluation';
 import { GameEventPayload } from '@common/game-event-payload';
 import { GameSnapshot } from '@common/game-snapshot';
 import { GameState } from '@common/game-state';
+import { Grade } from '@common/grade';
 import { Player } from '@common/player';
 import { PlayerState } from '@common/player-state';
+import { QrlEvaluation } from '@common/qrl-evaluation';
+import { QrlSubmission } from '@common/qrl-submission';
 import { Question } from '@common/question';
 import { Quiz } from '@common/quiz';
 import { Submission } from '@common/submission';
@@ -62,7 +65,7 @@ fdescribe('QrlBoardComponent', () => {
         description: 'A test quiz',
         duration: 30,
         lastModification: new Date(),
-        questions: [],
+        questions: [questionStub()[0]],
         isHidden: false,
         _id: 'quiz1',
     };
@@ -84,8 +87,15 @@ fdescribe('QrlBoardComponent', () => {
 
     beforeEach(() => {
         mockGameHttpService = jasmine.createSpyObj('GameHttpService', ['getGameSnapshotByPin']);
-        mockGameService = jasmine.createSpyObj('GameService', ['qrlSubmit', 'nextQuestion', 'onNextQuestion', 'onSubmitChoices', 'playerAbandon']);
-        mockPlayerService = jasmine.createSpyObj('PlayerService', ['getCurrentPlayer']);
+        mockGameService = jasmine.createSpyObj('GameService', [
+            'qrlSubmit',
+            'nextQuestion',
+            'onNextQuestion',
+            'onQrlSubmit',
+            'onQrlEvaluate',
+            'qrlInputChange',
+        ]);
+        mockPlayerService = jasmine.createSpyObj('PlayerService', ['getCurrentPlayer', 'playerAbandon']);
         matDialogMock = jasmine.createSpyObj('MatDialog', ['open']);
         dialogRefSpy = jasmine.createSpyObj('MatDialogRef<ConfirmationDialogComponent>', ['afterClosed']);
         dialogRefSpy.afterClosed.and.returnValue(of(true));
@@ -94,7 +104,7 @@ fdescribe('QrlBoardComponent', () => {
         timerServiceMock = jasmine.createSpyObj('TimerService', ['onTimerTick']);
 
         TestBed.configureTestingModule({
-            imports: [MatSnackBarModule, RouterTestingModule, MatDialogModule],
+            imports: [MatSnackBarModule, RouterTestingModule, MatDialogModule, BrowserAnimationsModule],
             declarations: [QRLboardComponent, ConfirmationDialogComponent],
             providers: [
                 { provide: GameHttpService, useValue: mockGameHttpService },
@@ -132,9 +142,15 @@ fdescribe('QrlBoardComponent', () => {
         timerServiceMock.onTimerTick.and.callFake((pin, callback) => {
             return webSocketServiceSpy.on('timerTick', applyIfPinMatches(pin, callback));
         });
-        // mockGameService.onSubmitChoices.and.callFake((pin, callback) => {
-        //     return webSocketServiceSpy.on('submitChoices', applyIfPinMatches(pin, callback));
-        // });
+        mockGameService.onQrlSubmit.and.callFake((pin, callback) => {
+            return webSocketServiceSpy.on('qrlSubmit', applyIfPinMatches(pin, callback));
+        });
+        mockGameService.onQrlSubmit.and.callFake((pin, callback) => {
+            return webSocketServiceSpy.on('qrlSubmit', applyIfPinMatches(pin, callback));
+        });
+        mockGameService.onQrlEvaluate.and.callFake((pin, callback) => {
+            return webSocketServiceSpy.on('qrlEvaluate', applyIfPinMatches(pin, callback));
+        });
         fixture = TestBed.createComponent(QRLboardComponent);
         component = fixture.componentInstance;
         component.pin = '1234';
@@ -157,6 +173,7 @@ fdescribe('QrlBoardComponent', () => {
         component.ngOnInit();
         expect(mockGameHttpService.getGameSnapshotByPin).toHaveBeenCalled();
         expect(component['setupSubscriptions']).toHaveBeenCalledWith(dummyPin);
+        expect(mockGameService.qrlInputChange).toHaveBeenCalledWith(dummyPin, false);
     });
 
     it('should ngOnDestroy', () => {
@@ -181,25 +198,30 @@ fdescribe('QrlBoardComponent', () => {
     it('should setupSubscriptions', () => {
         const payload: GameEventPayload<Question> = { pin: '123', data: questionStub()[0] };
         const timerPayload: GameEventPayload<TimerPayload> = { pin: '123', data: { remainingTime: 0, eventType: TimerEventType.Question } };
-        const evaluationPayload: GameEventPayload<Evaluation> = { pin: '123', data: lastPlayerEvaluationStub() };
+        const qrlPayload: GameEventPayload<QrlEvaluation> = {
+            pin: '123',
+            data: { clientId: 'test', grade: Grade.Good, score: 10, isLast: true },
+        };
+        const qrlSubmission: GameEventPayload<QrlSubmission> = { pin: '123', data: { answer: 'tesstststs', clientId: 'test' } };
         spyOn(component, 'submitAnswer');
         mockPlayerService.getCurrentPlayer.and.returnValue(lastPlayerEvaluationStub().player);
         component.pin = '123';
         component['setupSubscriptions']('123');
         socketServerMock.emit('nextQuestion', payload);
-        socketServerMock.emit('qrlSubmit', evaluationPayload);
+        socketServerMock.emit('qrlEvaluate', qrlPayload);
         socketServerMock.emit('timerTick', timerPayload);
+        socketServerMock.emit('qrlSubmit', qrlSubmission);
         expect(mockGameService.onNextQuestion).toHaveBeenCalled();
-        // expect(mockGameService.onSubmitChoices).toHaveBeenCalled();
+        expect(mockGameService.onQrlEvaluate).toHaveBeenCalled();
+        expect(mockGameService.onQrlSubmit).toHaveBeenCalled();
         expect(timerServiceMock.onTimerTick).toHaveBeenCalled();
         expect(component.submitAnswer).toHaveBeenCalled();
     });
 
-    it('should send a message and clear input when valid', () => {
+    it('should send a message when valid', () => {
         component.input = 'Test message';
         component.submitAnswer();
         expect(mockGameService.qrlSubmit).toHaveBeenCalledWith('1234', component.input);
-        expect(component.input).toBe('');
     });
 
     it('should not send a message if input is only whitespace', () => {
@@ -208,7 +230,7 @@ fdescribe('QrlBoardComponent', () => {
         expect(mockGameService.qrlSubmit).not.toHaveBeenCalled();
     });
 
-    it('should validate message as invalid if it contains only whitespace', () => {
+    it('should treat message as invalid if it contains only whitespace', () => {
         const messageControl = component.formGroup.get('message');
         if (messageControl) {
             messageControl.setValue('   ');
