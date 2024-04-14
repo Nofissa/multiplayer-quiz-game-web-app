588ac71ea96f01fc3b2461da0eeae746dd6f5d36
R0ma01 <romaine.brand@polymtl.ca>
2024-04-14 14:16:58 -0400

    temporary fix of the host game page and barchart layouts

--- client/src/app/interfaces/game-transition-display-options.ts
+++ client/src/app/interfaces/game-transition-display-options.ts
@@ -2,5 +2,6 @@ export interface GameTransitionDisplayOptions {
     hideTimerOnStartingGame?: boolean;
     hideTimerOnLoadingNextQuestion?: boolean;
     keepDisplayingCurrentQuestion?: boolean;
+    dontDisplayTextArea?: boolean;
     transitionalOnly?: boolean;
 }
