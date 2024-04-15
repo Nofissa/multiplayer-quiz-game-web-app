53d32de7a706d013be0bb6777e7380f5f1246973
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-04-15 19:33:30 -0400

    hotfix: moved validation values from enum to constant export

---
+++ server/app/constants/validation-values.ts
@@ -0,0 +1,9 @@
+export const VALIDATION_VALUES = {
+    minPoints: 10,
+    maxPoints: 100,
+    multipleOfPoints: 10,
+    minDuration: 10,
+    maxDuration: 60,
+    minAnswersSize: 2,
+    maxAnswersSize: 4,
+};
