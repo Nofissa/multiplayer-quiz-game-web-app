0b67bd4af9f263d4fa27c23506218de9ce01e665
Imed-Bennour <imed-eddine-2.bennour@polymtl.ca>
2024-04-16 16:42:56 -0400

    changed path sound

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
-import { IEnvironment } from './ienvironment';
 import { PROD_ENVIRONMENT } from '@app/constants/prod-environment';
+import { IEnvironment } from './ienvironment';
 
 export const environment: IEnvironment = {
     production: true,
     serverPort: PROD_ENVIRONMENT.serverPort,
     serverUrl: `${PROD_ENVIRONMENT.serverUrl}:${PROD_ENVIRONMENT.serverPort}`,
     apiUrl: `${PROD_ENVIRONMENT.serverUrl}:${PROD_ENVIRONMENT.serverPort}/api`,
-    panicAudioSrc: 'https://imed-bennour.github.io/sound-for-LOG2990/ticking-timer.wav',
+    panicAudioSrc: 'https://imed-bennour.github.io/logo-for-game-log2990/ticking-timer.mp3',
 };
